98cc97806e1fbd52f35f11fc1240c75788a661ed
MSuFleming <114168605+MSuFleming@users.noreply.github.com>
2023-04-05 15:45:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let map = new L.map('map', mapOptions);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var Routes= L.esri.featureLayer({url:'https://services1.arcgis.com/pMeXRvgWClLJZr3s/arcgis/rest/services/Evacuation_Routes/FeatureServer/0/'
+}).addTo(map);
